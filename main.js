044e2b845846dcc5836e39108d308b45bab3a97c
valladaresassc <36423652+valladaresassc@users.noreply.github.com>
2021-11-16 21:24:32 -0800

    Add files via upload
    Artist done...I think. 
    Updated main to test the Artist class

---
+++ main.js
@@ -0,0 +1,35 @@
+const {Painting} = require('./TestPainting.js');
+const {Buyer} = require('./Buyer.js');
+const {Artist} = require('./Artist.js');
+
+let squid = new Buyer("Squidward Tentacles", "STentacles", "18234 Bikini Bottom Road", "iamthebestmusician");
+let sponge = new Buyer("Spongebob Squarepants", "SSPants", "18235 Bikini Bottom Road", "squidwardisthebest");
+
+let MonaLisa = new Painting("Mona Lisa", 40000, 40000);
+let Scream = new Painting("Scream", 25000, 12000);
+let ObamaPortrait = new Painting("Painting of Obama", 100000, 0);
+let LincolnPortrait = new Painting("Painting of Abraham Lincoln", 20000000, 0);
+let boldandBrash = new Painting("Bold and Brash", 99999999999999, 10, "Squidward", "20x10", "Oil", 10000);
+
+let vanGogh = new Artist("Vincent Van Gogh", "leftear", "iminsane" );
+
+squid.insertOrders(boldandBrash);
+squid.insertOrders(MonaLisa);
+//squid.setBidder(3000000, ObamaPortrait);
+squid.printBuyer();
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+sponge.printBuyer();
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+// squid.setBidder(50, ObamaPortrait);
+console.log(squid.getOrderList());
+console.log(squid.getBidList());
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+vanGogh.printArtist();
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+vanGogh.addPainting(boldandBrash);
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+vanGogh.printArtist();
+vanGogh.knowsBidder();
+
+//vanGogh.setPrice();
+
